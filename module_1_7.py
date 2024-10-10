0679022e29e5a63fed0c88809cff6da9b8003e6e
IldarTukhfatullin <160497765+IldarTukhfatullin@users.noreply.github.com>
2024-10-10 15:33:42 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,8 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students_list = sorted(students)
+
+average_grades = dict(zip(students_list, [sum(grade) / len(grade) for grade in grades]))
+
+print(average_grades)
